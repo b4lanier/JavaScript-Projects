cdc4534f87cf7401e3db9f66d17b90533f203b71
b4lanier <104937523+b4lanier@users.noreply.github.com>
2022-06-07 06:50:40 -0600

    Basic Javascript projects created

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+window.alert("Hello world!");
+document.write("Hello, world");
+var A = "This is a string";
+document.write(A);                                  //assign a variable
+window.alert(A);
+window.alert("bart\'s \"skateboard\" is blue.");    //print "'s"
+var B = "Concatenated" + "String"                   //concatinate declaring variables
+document.write(B+A)                                 //concatinate printing variables
+var family = "Jones", dad = "Phil", mom = "Sally";  //assign multiple variables in a row
+document.write(mom);
+document.write(2 + 2);                              //write an expression
